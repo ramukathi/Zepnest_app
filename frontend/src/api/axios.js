f05c7b2318423b4e53cb0b1c8d75c717cbb39ebb
ramukathi <ramukathi9491@gmail.com>
2026-05-23 16:27:29 +0530

    Fixed frontend backend API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -6,7 +6,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://zepnestapp-production.up.railway.app/api",
+    "https://zepnestapp-production.up.railway.app",
 
   headers: {
     "Content-Type": "application/json",
