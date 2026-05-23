e57863dcbc7c376f65fdb3ca9a2e9f5e54a246eb
ramukathi <ramukathi9491@gmail.com>
2026-05-23 16:55:08 +0530

    Fixed mysql promise connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,38 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("❌ MySQL Connection Failed:", err.message);
-  } else {
-    console.log("✅ MySQL Connected Successfully");
+// TEST CONNECTION
+
+(async () => {
+  try {
+
+    const connection =
+      await pool.getConnection();
+
+    console.log(
+      "✅ MySQL Connected Successfully"
+    );
+
+    connection.release();
+
+  } catch (err) {
+
+    console.error(
+      "❌ MySQL Connection Failed:",
+      err.message
+    );
   }
-});
+})();
 
-module.exports = db;
+module.exports = pool;
